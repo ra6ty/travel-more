9e73a02649dcce05939eac32bebaaa093d79c6d2
ra6ty <sviat.t123@gmail.com>
2021-12-14 09:18:03 +0200

    add-adaptive

---
+++ skript.js
@@ -0,0 +1,9 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+let body = document.querySelector('body')
+
+menuBtn.addEventListener('click', function() {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    body.classList.toggle('lock')
+})
